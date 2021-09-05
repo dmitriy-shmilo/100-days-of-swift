ef6b3f37c17c825547ae8e9f5f6ebe08b946ddbd
Dmitriy Shmilo <dmitriy.shmilo@gmail.com>
2021-09-05 22:17:15 +0300

    Project 10.

---
+++ Project10/Project10/PersonViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PersonViewCell.swift
+//  Project10
+//
+//  Created by Dmitriy Shmilo on 05.09.2021.
+//
+
+import UIKit
+
+class PersonViewCell: UICollectionViewCell {
+	@IBOutlet var imageView: UIImageView!
+	@IBOutlet var nameLabel: UILabel!
+}
