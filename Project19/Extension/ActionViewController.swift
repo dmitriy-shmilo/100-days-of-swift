31d67ce0383966795bceac7b7dc3eb651b439fb2
Dmitriy Shmilo <dmitriy.shmilo@gmail.com>
2021-09-13 14:37:56 +0300

    Project 19, challenges.

--- Project19/Extension/ActionViewController.swift
+++ Project19/Extension/ActionViewController.swift
@@ -12,6 +12,8 @@ class ActionViewController: UIViewController {
 
     @IBOutlet weak var scriptTextView: UITextView!
 	
+	var currentScript = Script(id: UUID(), title: "Untitled", body: "")
+	var scripts = [Script]()
 	var pageTitle = ""
 	var pageURL = ""
 
@@ -26,8 +28,16 @@ class ActionViewController: UIViewController {
 					self?.pageTitle = values["title"] as? String ?? ""
 					self?.pageURL = values["URL"] as? String ?? ""
 					
+					if let data = UserDefaults.standard.data(forKey: "scripts") {
+						if let decoded = try? JSONDecoder().decode([Script].self, from: data) {
+							self?.scripts = decoded
+							self?.currentScript = self?.scripts[0] ?? Script(id: UUID(), title: "Untitled", body: "")
+						}
+					}
+					
 					DispatchQueue.main.async {
-						self?.title = self?.pageTitle
+						self?.title = self?.currentScript.title
+						self?.scriptTextView.text = self?.currentScript.body
 					}
 				}
 			}
@@ -38,13 +48,57 @@ class ActionViewController: UIViewController {
     }
 
     @IBAction func done() {
+		self.currentScript.body = self.scriptTextView.text
+		self.scripts.removeAll { $0.id == currentScript.id }
+		self.scripts.append(currentScript)
+		self.save()
+		
         let item = NSExtensionItem()
-		let arg: NSDictionary = ["customJavaScript": scriptTextView.text ?? ""]
+		let arg: NSDictionary = ["customJavaScript": self.scriptTextView.text ?? ""]
 		let finalizeArg: NSDictionary = [NSExtensionJavaScriptFinalizeArgumentKey: arg]
 		let customJavascript = NSItemProvider(item: finalizeArg, typeIdentifier: kUTTypePropertyList as String)
 		item.attachments = [customJavascript]
 		extensionContext?.completeRequest(returningItems: [item])
     }
+	
+	@IBAction func rename(_ sender: Any) {
+		let alert = UIAlertController(title: "Rename script", message: nil, preferredStyle: .alert)
+		alert.addTextField()
+		alert.textFields?[0].text = self.currentScript.title
+		alert.addAction(UIAlertAction(title: "Rename", style: .default) { [weak self, weak alert] _ in
+			self?.currentScript.title = alert?.textFields?[0].text ?? "Untitled"
+			self?.scripts.removeAll { $0.id == self?.currentScript.id }
+			if let script = self?.currentScript {
+				self?.scripts.append(script)
+				self?.title = script.title
+				self?.save()
+			}
+		})
+		alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+		present(alert, animated: true)
+	}
+	
+	@IBAction func load(_ sender: Any) {
+		let sheet = UIAlertController(title: "Load script", message: nil, preferredStyle: .actionSheet)
+		self.scripts.map { script in
+			UIAlertAction(title: script.title, style: .default, handler: { [weak self] _ in
+				self?.load(script: script)
+			})
+		}.forEach {
+			sheet.addAction($0)
+		}
+		sheet.addAction(UIAlertAction(title: "Create New", style: .default, handler: { [weak self] _ in
+			self?.save()
+			self?.currentScript = Script(id: UUID(), title: "Untitled", body: "// new script")
+			
+			if let script = self?.currentScript {
+				self?.title = script.title
+				self?.scriptTextView.text = script.body
+			}
+		}))
+		sheet.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+		present(sheet, animated: true)
+	}
 	
 	@objc func adjustforKeyboard(notification: Notification) {
 		guard let kbValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else {
@@ -63,5 +117,18 @@ class ActionViewController: UIViewController {
 		let selectedRange = scriptTextView.selectedRange
 		scriptTextView.scrollRangeToVisible(selectedRange)
 	}
-
+	
+	private func load(script: Script) {
+		self.currentScript = script
+		self.scriptTextView.text = script.body
+		self.title = script.title
+	}
+	
+	private func save() {
+		DispatchQueue.global().async {
+			if let data = try? JSONEncoder().encode(self.scripts) {
+				UserDefaults.standard.set(data, forKey: "scripts")
+			}
+		}
+	}
 }
---
+++ Project19/Extension/Script.swift
@@ -0,0 +1,14 @@
+//
+//  Script.swift
+//  Extension
+//
+//  Created by Dmitriy Shmilo on 12.09.2021.
+//
+
+import Foundation
+
+struct Script: Codable {
+	var id: UUID
+	var title: String
+	var body: String
+}
