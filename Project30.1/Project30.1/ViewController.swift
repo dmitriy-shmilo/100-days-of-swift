43ae7fdbe4882ee9fef90096b0945c93186d48f0
Dmitriy Shmilo <dmitriy.shmilo@gmail.com>
2021-10-31 21:34:08 +0200

    Project 30.1, WIP1.

---
+++ Project30.1/Project30.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project30.1
+//
+//  Created by Dmitriy Shmilo on 22.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
